3d7c84a0a7f8d58313ded10305bd88ff2488ca5b
IskaNeumann <franziska.neumann@live.de>
2017-05-10 19:41:17 +0200

    replace totalAmount with Query

--- src/Customer.java
+++ src/Customer.java
@@ -41,9 +41,22 @@ class Customer {
             totalAmount += each.getCharge();
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
     }
+
+    private double getTotalCharge()
+    {
+        double result = 0;
+        Enumeration enum_rentals = rentals.elements();
+        while(enum_rentals.hasMoreElements())
+        {
+            Rental each = (Rental) enum_rentals.nextElement();
+            result += each.getCharge();
+        }
+        return result;
+    }
+
 }
     
